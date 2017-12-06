ab4ed0b870808d53dd0d79125993b199fa1c6ec8
Conor <ckosidowski11@winona.edu>
2017-12-05 21:30:49 -0600

    Running to get an output for comparison

--- test/testGenerator/outputClasses/TestComputeCommission.java
+++ test/testGenerator/outputClasses/TestComputeCommission.java
@@ -45,48 +45,84 @@ public class TestComputeCommission
     @Test
     public void test5()
     {
-        mockComputeCommission.findTotalCommission(39, 1, 50);
+        mockComputeCommission.findTotalCommission(0, 44, 50);
     }
 
     @Test
     public void test6()
     {
-        mockComputeCommission.findTotalCommission(39, 2, 50);
+        mockComputeCommission.findTotalCommission(78, 44, 50);
     }
 
     @Test
     public void test7()
     {
-        mockComputeCommission.findTotalCommission(39, 87, 50);
+        mockComputeCommission.findTotalCommission(39, 1, 50);
     }
 
     @Test
     public void test8()
     {
-        mockComputeCommission.findTotalCommission(39, 88, 50);
+        mockComputeCommission.findTotalCommission(39, 2, 50);
     }
 
     @Test
     public void test9()
     {
-        mockComputeCommission.findTotalCommission(39, 44, 1);
+        mockComputeCommission.findTotalCommission(39, 87, 50);
     }
 
     @Test
     public void test10()
     {
-        mockComputeCommission.findTotalCommission(39, 44, 2);
+        mockComputeCommission.findTotalCommission(39, 88, 50);
     }
 
     @Test
     public void test11()
     {
-        mockComputeCommission.findTotalCommission(39, 44, 98);
+        mockComputeCommission.findTotalCommission(39, 0, 50);
     }
 
     @Test
     public void test12()
     {
+        mockComputeCommission.findTotalCommission(39, 89, 50);
+    }
+
+    @Test
+    public void test13()
+    {
+        mockComputeCommission.findTotalCommission(39, 44, 1);
+    }
+
+    @Test
+    public void test14()
+    {
+        mockComputeCommission.findTotalCommission(39, 44, 2);
+    }
+
+    @Test
+    public void test15()
+    {
+        mockComputeCommission.findTotalCommission(39, 44, 98);
+    }
+
+    @Test
+    public void test16()
+    {
         mockComputeCommission.findTotalCommission(39, 44, 99);
+    }
+
+    @Test
+    public void test17()
+    {
+        mockComputeCommission.findTotalCommission(39, 44, 0);
+    }
+
+    @Test
+    public void test18()
+    {
+        mockComputeCommission.findTotalCommission(39, 44, 100);
     }
 }
